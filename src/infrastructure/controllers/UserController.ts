fa918d95e5057b94d5378700b45161235f973df3
m-housni <mohamedhousni1000k@gmail.com>
2025-03-12 13:53:25 +0000

    Add user routes and update UserController to handle user creation

--- src/infrastructure/controllers/UserController.ts
+++ src/infrastructure/controllers/UserController.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
 import { container } from "../../config/container";
 import { CreateUser } from "../../application/use-cases/CreateUser";
+import { User } from "../../domain/User";
 
 export class UserController {
-    static async createUser(req: Request, res: Response) {
+    // TODO: handle the any type for the response
+    static async createUser(req: Request, res: Response): Promise<any> {
         const { name, email } = req.body;
         const createUser = container.resolve(CreateUser);
         const user = await createUser.execute(name, email);
