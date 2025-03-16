0b902a99547ca86316ed290a2e3a27eee7c580ef
m-housni <mohamedhousni1000k@gmail.com>
2025-03-16 13:35:02 +0000

    Initialize database connection in server setup

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import userRoutes from "./infrastructure/routes/userRoutes";
 import "reflect-metadata";
-import dotenv from "dotenv";
 
+import dotenv from "dotenv";
 dotenv.config();
+
+import connectDB from "./infrastructure/database/config";
+connectDB();
 
 const app = express();
 app.use(express.json());
